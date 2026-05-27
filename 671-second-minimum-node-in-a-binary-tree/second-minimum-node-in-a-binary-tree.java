a3f70f4ae42e085b6f1e54f429cc8f1a93edc820
Kishan Sharma <140365189+16Kishan@users.noreply.github.com>
2026-05-27 18:18:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (77.40%) - LeetSync

---
+++ 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.java
@@ -0,0 +1,19 @@
+
+class Solution {
+   public int findSecondMinimumValue(TreeNode root) {
+	if(root==null || root.right==null || root.left==null) return -1;
+	Queue<TreeNode> q = new LinkedList<>();
+	q.offer(root);
+	Integer secondMin = null;
+	while(!q.isEmpty()) {
+		TreeNode curr = q.poll();
+		if(curr.right!=null) q.offer(curr.right);
+		if(curr.left!=null) q.offer(curr.left);
+		if(curr.val!=root.val) {
+			if(secondMin==null) secondMin = curr.val;
+			else secondMin = Math.min(secondMin, curr.val);
+		}
+	}
+	return secondMin==null? -1 : secondMin;
+}
+}
