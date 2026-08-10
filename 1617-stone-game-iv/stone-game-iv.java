7e3b10d1c04982513216e51cbb34c25d9a2c9c82
Kishan Sharma <140365189+16Kishan@users.noreply.github.com>
2026-08-10 19:34:30 +0530

    Time: 3 ms (99.74%) | Memory: 42.5 MB (68.41%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -0,0 +1,18 @@
+class Solution {
+    
+    public boolean winnerSquareGame(int n) {
+        
+        boolean dp[] = new boolean[n + 1];
+        
+        for(int i = 0; i <= n; i++){
+            
+            if(dp[i]) continue;
+            
+            for(int j = 1; i + j * j <= n; j++)
+                dp[i + j * j] = true;
+        }
+        
+        // System.out.println(Arrays.toString(dp));
+        return dp[n];
+    }
+}
